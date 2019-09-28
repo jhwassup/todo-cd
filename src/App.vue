e0e8c77ab952d53088afe7cf272769f5ca8875cc
jhwassup <cyon0610@gmail.com>
2019-09-28 15:27:53 +0900

    init project

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <todo-header></todo-header>
+    <todo-input v-on:add="addTodoItem"></todo-input>
+    <!-- TODO: remove 버튼 구현해보세요 -->
+    <todo-list 
+      v-bind:items="todoItems"
+      v-on:remove="removeTodoItems"
+    ></todo-list>
+    <todo-footer v-on:clear="clearTodoItems"></todo-footer>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+	components: {
+		TodoHeader,
+		// TodoInput: () => import('./components/TodoInput.vue'),
+		TodoInput,
+		TodoList,
+		TodoFooter,
+	},
+	data() {
+		return {
+			todoItems: [],
+		};
+	},
+	methods: {
+		addTodoItem: function(value) {
+			this.todoItems.push(value);
+			localStorage.setItem(value, value);
+		},
+		removeTodoItems: function(todoItem, index) {
+			this.todoItems.splice(index, 1);
+			localStorage.removeItem(todoItem);
+		},
+		fetchTodoItems: function() {
+			for (var i = 0; i < localStorage.length; i++) {
+				var item = localStorage.key(i);
+				this.todoItems.push(item);
+			}
+		},
+		clearTodoItems: function() {
+			this.todoItems = [];
+			localStorage.clear();
+		},
+	},
+	created: function() {
+		this.fetchTodoItems();
+	},
+};
+</script>
+
+<style>
+</style>
